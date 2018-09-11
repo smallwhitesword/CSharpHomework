c2fdf73d976ada033f048ba8fc36bb3293111551
smallwhitesword <1950980360@qq.com>
2018-09-11 23:33:50 +0800

    添加项目文件。

---
+++ Homework1/program1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class program1
+    {
+        static void AppLine(string[] args)
+        {
+            string s = "";
+            int a = 0;
+            int b = 0;
+            int result;
+            Console.Write("Please input an number: ");
+            s = Console.ReadLine();
+            a = Int32.Parse(s);
+            Console.Write("Please input an number: ");
+            s = Console.ReadLine();
+            b = Int32.Parse(s);
+            result = a * b;
+            Console.Write("两个数的积为：" + result);
+
+        }
+    }
+}
---
+++ Homework1/program2.Designer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+namespace Homework1
+
+{
+    partial class program2:Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // program2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 434);
+            this.Name = "program2";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        TextBox txt1 = new TextBox();
+        TextBox txt2 = new TextBox();
+        Button btn = new Button();
+        Label lbl = new Label();
+        public void init()
+        {
+            this.Controls.Add(txt1);
+            this.Controls.Add(txt2);
+            this.Controls.Add(btn);
+            this.Controls.Add(lbl);
+            txt1.Dock = System.Windows.Forms.DockStyle.Left;
+            txt2.Dock = System.Windows.Forms.DockStyle.Right;
+            btn.Dock = System.Windows.Forms.DockStyle.Fill;
+            lbl.Dock = System.Windows.Forms.DockStyle.Bottom;
+            btn.Text = "两个数字的积是：";
+            lbl.Text = "用于显示结果的标签";
+            btn.Click += new System.EventHandler(this.buttonl_Clink);
+        }
+        public void buttonl_Clink(object sender,EventArgs e)
+        {
+            string s = txt1.Text;
+            int a = Int32.Parse(s);
+            s = txt2.Text;
+            int b =Int32.Parse(s);
+            int result = a * b;
+            lbl.Text = "两数乘积为：" + result;
+        }
+        static void Main()
+        {
+            program2 i = new program2();
+            i.Text = "program2";
+            i.init();
+            Application.Run(i);
+        }
+    }
+}
